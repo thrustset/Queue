12e2781c97e5be40ba886e70aa89a50b3222d448
Nikolay <thrust_set@vk.com>
2022-03-09 13:06:22 +0300

    v1

--- main.cpp
+++ main.cpp
@@ -3,19 +3,16 @@
 
 int main() {
     auto queue = Queue();
-
     std::cout << queue << "\n";
 
     for(int i = 0; i <= MAXSIZE; i++) {
         queue.push(i + 1);
         std::cout << queue << "\n";
     }
-
     for(int i = 0; i < MAXSIZE / 2; i++) {
         queue.pop();
         std::cout << queue << "\n";
     }
-
     for(int i = 0; i < MAXSIZE / 2; i++) {
         queue.push(i + 1);
         std::cout << queue << "\n";
